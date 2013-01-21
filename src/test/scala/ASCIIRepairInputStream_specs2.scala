c963ae2e6c4c152a69791fe5f5deb246b989804e
Mikko Östlund <mikost@server.fake>
2013-01-21 17:18:18 +0100

    src/test/scala/ASCIIRepairFilterInputStream_specs2.scala: Preparing for move to src/test/scala/name/mikkoostlund/utils/logfetcher/ASCIIRepairFilterInputStreamSpecTest.scala

--- src/test/scala/ASCIIRepairInputStream_specs2.scala
+++ src/test/scala/ASCIIRepairInputStream_specs2.scala
@@ -1,40 +1,44 @@
-import org.specs2.mutable._
-import name.mikkoostlund.utils.logfetcher.ASCIIRepairFilterInputStream
-import java.io.InputStream
-import org.specs2.matcher.MatchResult
-import java.io.ByteArrayInputStream
+package name.mikkoostlund.utils.logfetcher
 
+import org.specs2._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+
+@RunWith(classOf[JUnitRunner])
 class Spec2Test extends Specification {
 
-  val brokenASCIIBytes = {
-    "ABCD".getBytes.
-      updated(1, 127.toByte).   // insert non-ASCII
-      updated(2, 255.toByte)    // insert non-ASCII
+  trait TestData extends Scope {
+    val brokenASCIIBytes = {
+      "ABCD".getBytes.
+        updated(1, 127.toByte).   // insert non-ASCII
+        updated(2, 255.toByte)    // insert non-ASCII
+    }
+  
+    val brokenInputStream: InputStream = new ByteArrayInputStream(brokenASCIIBytes)
+    val repairValue: Byte = '$'
   }
 
   "An ASCIIRepairFilterInputStream" should {
-    "replace non-ASCII chars with dollar-signs" in {
-
-      val brokenInputStream = new ByteArrayInputStream(brokenASCIIBytes)
-      val repairValue: Byte = '$'
+    "replace non-ASCII chars with dollar-signs" in new TestData {
       val repairedStream = new ASCIIRepairFilterInputStream(brokenInputStream, repairValue)
 
       repairedStream.read mustEqual 'A'
-      repairedStream.read mustEqual '$'
-      repairedStream.read mustEqual '$'
+      repairedStream.read mustEqual repairValue
+      repairedStream.read mustEqual repairValue
       repairedStream.read mustEqual 'D'
       repairedStream.read mustEqual -1   // End-of-file
+      success
     }
 
-    "replace non-ASCII chars with dollar-signs" in {
-
-      val brokenInputStream = new ByteArrayInputStream(brokenASCIIBytes)
-      val repairValue: Byte = '$'
+    "replace non-ASCII chars with dollar-signs" in new TestData {
       val repairedStream = new ASCIIRepairFilterInputStream(brokenInputStream, repairValue)
       val repairedASCIIBytes = Array.ofDim[Byte](4)
-      repairedStream.read(repairedASCIIBytes, 0, 4)
-      repairedASCIIBytes mustEqual Array[Byte]('A','$','$','D')
+      repairedStream.read(repairedASCIIBytes, 0, 4) mustEqual 4
+      repairedASCIIBytes mustEqual Array[Byte]('A', repairValue, repairValue, 'D')
     }
-  }
-  
+  }  
 }
